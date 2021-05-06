de220d046f685622befb4b4c800ca2e99944f64b
wtsaygers1 <wtsaygers1@gmail.com>
2021-05-06 16:31:29 -0400

    sports table created

--- database/seeders/SportSeeder.php
+++ database/seeders/SportSeeder.php
@@ -14,17 +14,28 @@ class SportSeeder extends Seeder
      */
     public function run()
     {
-        Sport::create([
-            'sport_name' => 'spike ball'
-        ]);
-        Sport::create([
-            'sport_name' => 'corn hole'
-        ]);
-        Sport::create([
-            'sport_name' => 'kan jam'
-        ]);
-        Sport::create([
-            'sport_name' => 'pickleball'
-        ]);
+        $sport_names = [
+            'spike ball',
+            'corn hole',
+            'kan jam',
+            'pickleball'
+        ];
+        for($i = 0; $i<count($sport_names); $i++){
+            $sport = new Sport;
+            $sport->sport_name = $sport_names[$i];
+            $sport->save();
+        }
     }
 }
+                // Sport::create([
+                //     'sport_name' => 'spike ball'
+                // ]);
+                // Sport::create([
+                //     'sport_name' => 'corn hole'
+                // ]);
+                // Sport::create([
+                //     'sport_name' => 'kan jam'
+                // ]);
+                // Sport::create([
+                //     'sport_name' => 'pickleball'
+                // ]);
