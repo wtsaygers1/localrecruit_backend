8bea79f0d916d498e44086de19ccb17b84cfb34f
wtsaygers1 <wtsaygers1@gmail.com>
2021-05-11 22:39:13 -0400

    code freeze commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    
+
     protected $table = 'users';
     protected $primaryKey = 'id';
     public $incrementing = true;
@@ -37,5 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
-
+    public function authorBooks()
+    {
+        return $this->hasMany(UserSports::class);
+    }
 }
